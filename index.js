364087fbdee0fd569a1a238fb267bf57a932f4fb
way den <65202623+Nedyarg@users.noreply.github.com>
2022-10-13 03:09:16 +0000

    AasAS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser');
 var express = require('express');
-
+var logged = false
 
 const fs = require('fs');
 var app = express();
@@ -9,25 +9,35 @@ var PORT = 3000;
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 //post for login
+
 app.post('/submitlogin', (req, res) => {
     console.log(req.body);
     const data = fs.readFileSync('logins.txt', 'UTF-8')
 
     // split the contents by new line
     const lines = data.split(/\r?\n/)
-  
     // print all lines
+    logged = false
     lines.forEach(line => {
         if (req.body.password  == JSON.parse(line).password && req.body.username  == JSON.parse(line).username) {
-            console.log("logged IN")
-            res.redirect('/home.html')
+        
+            console.log("loggin in")
+            res.redirect('/home.html')     
+            logged = true
+                                              
+                                                                                                                 
         }
-        console.log(JSON.parse(line))
-    })
+
+  
     
+    
+     
+    })
+    if (logged === false){res.redirect('/incorrect.html')}
 })
+    
 app.post('/signup', (req, res) => {
-    res.redirect('/html.html');
+    res.redirect('/signupnotice.html');
     console.log(req.body);
     fs.appendFile('loginrequests.txt', JSON.stringify(req.body) + '\n' ,err => {
         if (err) {
